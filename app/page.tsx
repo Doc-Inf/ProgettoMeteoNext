af36795ed39f87606ee13a0bb3fb18e6b0c69c17
leo0o7 <giardinoleonardo34@gmail.com>
2024-04-29 19:47:40 +0200

    added home page data fetching (from Doc-Inf/ProgettoMeteo3 commit 2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,145 +1,401 @@
 "use client";
-import { useRef } from "react";
-
-import {
-  Tabs,
-  TabsList,
-  TabsContent,
-  TabsTrigger,
-} from "../components/ui/tabs";
-
-import { motion, useInView, useScroll } from "framer-motion";
-
-import { Cloud, CloudRain, Droplet, ParkingMeter } from "lucide-react";
-import Info from "@/components/cards/info";
 import HeroHeading from "@/components/headings/hero-heading";
-import InfoTabs from "@/components/infoTabs";
-import { Container, ContainerCols } from "@/components/container";
-import WeekOverview from "@/components/week-overview";
-import dynamic from "next/dynamic";
+import HeroWeek from "@/components/hero/hero-week-temp";
+import HeroOverview from "@/components/hero/hero-overview";
+import HeroGraphs from "@/components/hero/hero-graphs";
+import AnimationWrapper from "@/components/ui/anim-wrapper";
+import { useEffect, useRef, useState } from "react";
 
-const MyChart = dynamic(() => import("@/components/myChart"), { ssr: false });
 export default function Home() {
-  // scroll animations
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["0 2", "2 1"],
-  });
-  // menu onclick animation
+  // State to store fetched data
+  const [datiUltimaRilevazione, setDatiUltimaRilevazione] = useState(null);
+  const [temperaturaUltimaRilevazione, setTemperaturaUltimaRilevazione] =
+    useState(-1);
+  const [umiditaUltimaRilevazione, setUmiditaUltimaRilevazione] = useState(-1);
+  const [direzioneVentoUltimaRilevazione, setDirezioneVentoUltimaRilevazione] =
+    useState(-1);
+  const [pressioneUltimaRilevazione, setPressioneUltimaRilevazione] =
+    useState(-1);
+  const [
+    temperaturaGiornoPrimaUltimaRilevazione,
+    setTemperaturaGiornoPrimaUltimaRilevazione,
+  ] = useState(-1);
+  const [
+    umiditaGiornoPrimaUltimaRilevazione,
+    setUmiditaGiornoPrimaUltimaRilevazione,
+  ] = useState(-1);
+  const [
+    direzioneVentoGiornoPrimaUltimaRilevazione,
+    setDirezioneVentoGiornoPrimaUltimaRilevazione,
+  ] = useState(-1);
+  const [
+    pressioneGiornoPrimaUltimaRilevazione,
+    setPressioneGiornoPrimaUltimaRilevazione,
+  ] = useState(-1);
+  const [
+    variazioneTemperaturaUltimoGiornoMisurazione,
+    setVariazioneTemperaturaUltimoGiornoMisurazione,
+  ] = useState("");
+  const [
+    variazioneUmiditaUltimoGiornoMisurazione,
+    setVariazioneUmiditaUltimoGiornoMisurazione,
+  ] = useState("");
+  const [
+    variazionePressioneUltimoGiornoMisurazione,
+    setVariazionePressioneUltimoGiornoMisurazione,
+  ] = useState("");
+  const [giorniSettimanaCorrente, setGiorniSettimanaCorrente] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [giorniGrafico, setGiorniGrafico] = useState([
+    "01/01",
+    "02/01",
+    "03/01",
+    "04/01",
+    "05/01",
+    "06/01",
+    "07/01",
+  ]);
+  const [mesiUltimaSettimana, setMesiUltimaSettimana] = useState([
+    "GEN",
+    "GEN",
+    "GEN",
+    "GEN",
+    "GEN",
+    "GEN",
+    "GEN",
+  ]);
+  const [temperaturaSettimanale, setTemperaturaSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [umiditaSettimanale, setUmiditaSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [pressioneSettimanale, setPressioneSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [precipitazioniSettimanali, setPrecipitazioniSettimanali] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [maxTemperaturaSettimanale, setMaxTemperaturaSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [minTemperaturaSettimanale, setMinTemperaturaSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [maxUmiditaSettimanale, setMaxUmiditaSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [minUmiditaSettimanale, setMinUmiditaSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [maxPressioneSettimanale, setMaxPressioneSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [minPressioneSettimanale, setMinPressioneSettimanale] = useState([
+    0, 0, 0, 0, 0, 0, 0,
+  ]);
 
-  // load graph only when in view
-  const graph = useRef(null);
-  const IsInViewGraph = useInView(graph);
+  // Effect to fetch data when the component mounts
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
+  // Function to fetch data
+  const fetchData = async () => {
+    try {
+      // Make a GET request using the Fetch API
+      const response = await fetch(
+        "http://localhost:80/TestMeteo3/php/datiHome.php"
+      );
+      //const response = await fetch('https://www.itisvallauri.net/meteo3//php/datiHome.php');
+
+      // Check if the response is successful (status code 200-299)
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      } else {
+        console.log(
+          "OK abbiamo preso i dati, o almeno così sembra dalla risposta"
+        );
+      }
+
+      // Parse the JSON data from the response
+      let json = await response.json();
+      console.log(json);
+      // Update the state with the fetched data
+      setDatiUltimaRilevazione(json);
+      console.log(datiUltimaRilevazione);
+
+      const ultimaRilevazione = json["ultimaRilevazione"];
+      const rilevazioneGiornoPrimaUltima = json["rilevazioneGiornoPrimaUltima"];
+      const giorniSettimanaCorrente = json["giorniSettimanaCorrente"];
+      const temperaturaSettimanale = json["temperaturaSettimanale"];
+      const umiditaSettimanale = json["umiditaSettimanale"];
+      const pressioneSettimanale = json["pressioneSettimanale"];
+      const pioggiaSettimanale = json["pioggiaSettimanale"];
+      const maxTemperaturaSettimanale = json["maxTemperaturaSettimanale"];
+      const minTemperaturaSettimanale = json["minTemperaturaSettimanale"];
+      const maxUmiditaSettimanale = json["maxUmiditaSettimanale"];
+      const minUmiditaSettimanale = json["minUmiditaSettimanale"];
+      const maxPressioneSettimanale = json["maxPressioneSettimanale"];
+      const minPressioneSettimanale = json["minPressioneSettimanale"];
+
+      let nullToZero = (array: Array<number>) => {
+        for (let i = 0; i < array.length; ++i) {
+          if (array[i] == null) {
+            array[i] = 0;
+          } else {
+            array[i] = Number(("" + array[i]).replace(",", ""));
+          }
+        }
+        return array;
+      };
+
+      console.log(
+        "Giorni della settimana corrente: " + giorniSettimanaCorrente
+      );
+      console.log(
+        "Temperature Settimanali: " + nullToZero(temperaturaSettimanale)
+      );
+      console.log("Umidità Settimanale: " + nullToZero(umiditaSettimanale));
+      console.log("Pressione Settimanale: " + nullToZero(pressioneSettimanale));
+      console.log("Pioggia Settimanale: " + nullToZero(pioggiaSettimanale));
+
+      const gxg: string[] = []; // array temporaneo per i giorni del grafico
+      const extractGiorni = (giorni: string[], array: string[]) => {
+        for (let i = 0; i < giorni.length; ++i) {
+          const d = ("" + giorni[i]).split("-");
+          array.push(d[2] + "/" + d[1]);
+        }
+        return array;
+      };
+
+      setGiorniSettimanaCorrente(giorniSettimanaCorrente);
+      setGiorniGrafico(extractGiorni(giorniSettimanaCorrente, gxg));
+      setTemperaturaSettimanale(nullToZero(temperaturaSettimanale));
+      setUmiditaSettimanale(nullToZero(umiditaSettimanale));
+      setPressioneSettimanale(nullToZero(pressioneSettimanale));
+      setPrecipitazioniSettimanali(nullToZero(pioggiaSettimanale));
+      setMaxTemperaturaSettimanale(nullToZero(maxTemperaturaSettimanale));
+      setMinTemperaturaSettimanale(nullToZero(minTemperaturaSettimanale));
+      setMaxUmiditaSettimanale(nullToZero(maxUmiditaSettimanale));
+      setMinUmiditaSettimanale(nullToZero(minUmiditaSettimanale));
+      setMaxPressioneSettimanale(nullToZero(maxPressioneSettimanale));
+      setMinPressioneSettimanale(nullToZero(minPressioneSettimanale));
+
+      setTemperaturaUltimaRilevazione(
+        ultimaRilevazione["temperaturaUltimaRilevazione"]
+      );
+      setUmiditaUltimaRilevazione(
+        ultimaRilevazione["umiditaUltimaRilevazione"]
+      );
+      setDirezioneVentoUltimaRilevazione(
+        ultimaRilevazione["direzioneVentoUltimaRilevazione"]
+      );
+      setPressioneUltimaRilevazione(
+        ultimaRilevazione["pressioneUltimaRilevazione"]
+      );
+
+      setTemperaturaGiornoPrimaUltimaRilevazione(
+        rilevazioneGiornoPrimaUltima["temperaturaUltimaRilevazione"]
+      );
+      setUmiditaGiornoPrimaUltimaRilevazione(
+        rilevazioneGiornoPrimaUltima["umiditaUltimaRilevazione"]
+      );
+      setDirezioneVentoGiornoPrimaUltimaRilevazione(
+        rilevazioneGiornoPrimaUltima["direzioneVentoUltimaRilevazione"]
+      );
+      setPressioneGiornoPrimaUltimaRilevazione(
+        rilevazioneGiornoPrimaUltima["pressioneUltimaRilevazione"]
+      );
+
+      if (
+        ultimaRilevazione["temperaturaUltimaRilevazione"] != null &&
+        rilevazioneGiornoPrimaUltima["temperaturaUltimaRilevazione"] != null
+      ) {
+        const deltaTemperatura: number = Number(
+          (
+            ((ultimaRilevazione["temperaturaUltimaRilevazione"] -
+              rilevazioneGiornoPrimaUltima["temperaturaUltimaRilevazione"]) /
+              rilevazioneGiornoPrimaUltima["temperaturaUltimaRilevazione"]) *
+            100
+          ).toFixed(2)
+        );
+        if (deltaTemperatura > 0) {
+          setVariazioneTemperaturaUltimoGiornoMisurazione(
+            "+" + deltaTemperatura + "% rispetto a ieri"
+          );
+        } else {
+          setVariazioneTemperaturaUltimoGiornoMisurazione(
+            "" + deltaTemperatura + "% rispetto a di ieri"
+          );
+        }
+      } else {
+        setVariazioneTemperaturaUltimoGiornoMisurazione("");
+      }
+
+      if (
+        ultimaRilevazione["umiditaUltimaRilevazione"] != null &&
+        rilevazioneGiornoPrimaUltima["umiditaUltimaRilevazione"] != null
+      ) {
+        const deltaUmidita = Number(
+          (
+            ((ultimaRilevazione["umiditaUltimaRilevazione"] -
+              rilevazioneGiornoPrimaUltima["umiditaUltimaRilevazione"]) /
+              rilevazioneGiornoPrimaUltima["umiditaUltimaRilevazione"]) *
+            100
+          ).toFixed(2)
+        );
+        if (deltaUmidita > 0) {
+          setVariazioneUmiditaUltimoGiornoMisurazione(
+            "+" + deltaUmidita + "% rispetto a ieri"
+          );
+        } else {
+          setVariazioneUmiditaUltimoGiornoMisurazione(
+            "" + deltaUmidita + "% rispetto a ieri"
+          );
+        }
+      } else {
+        setVariazioneUmiditaUltimoGiornoMisurazione("");
+      }
+
+      if (
+        ultimaRilevazione["pressioneUltimaRilevazione"] != null &&
+        rilevazioneGiornoPrimaUltima["pressioneUltimaRilevazione"] != null
+      ) {
+        const deltaPressione = Number(
+          (
+            ((ultimaRilevazione["pressioneUltimaRilevazione"] -
+              rilevazioneGiornoPrimaUltima["pressioneUltimaRilevazione"]) /
+              rilevazioneGiornoPrimaUltima["pressioneUltimaRilevazione"]) *
+            100
+          ).toFixed(2)
+        );
+        if (deltaPressione > 0) {
+          setVariazionePressioneUltimoGiornoMisurazione(
+            "+" + deltaPressione + "% rispetto a di ieri"
+          );
+        } else {
+          setVariazionePressioneUltimoGiornoMisurazione(
+            "" + deltaPressione + "% rispetto a di ieri"
+          );
+        }
+      } else {
+        setVariazionePressioneUltimoGiornoMisurazione("");
+      }
+
+      const getMese = (giorno: string) => {
+        const d = giorno.split("-");
+        switch (d[1]) {
+          case "01":
+          case "1": {
+            return "GEN";
+          }
+          case "02":
+          case "2": {
+            return "FEB";
+          }
+          case "03":
+          case "3": {
+            return "MAR";
+          }
+          case "04":
+          case "4": {
+            return "APR";
+          }
+          case "05":
+          case "5": {
+            return "MAG";
+          }
+          case "06":
+          case "6": {
+            return "GIU";
+          }
+          case "07":
+          case "7": {
+            return "LUG";
+          }
+          case "08":
+          case "8": {
+            return "AGO";
+          }
+          case "09":
+          case "9": {
+            return "SET";
+          }
+          case "10": {
+            return "OTT";
+          }
+          case "11": {
+            return "NOV";
+          }
+          case "12": {
+            return "DIC";
+          }
+          default: {
+            return "Mese non disponibile";
+          }
+        }
+      };
+      let ms = [];
+      for (let i = 0; i < giorniSettimanaCorrente.length; ++i) {
+        ms.push(getMese(giorniSettimanaCorrente[i]));
+      }
+      setMesiUltimaSettimana(ms);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const ref = useRef(null);
 
   return (
     <>
-      <HeroHeading />
-      {/* CARDS OVERVIEW */}
-      <motion.div
-        ref={ref}
-        style={{
-          scale: scrollYProgress,
-        }}
-      >
-        <ContainerCols>
-          <InfoTabs
-            tabs={[
-              { key: "Minima", value: 10.2 },
-              { key: "Media", value: 15.2 },
-              { key: "Massima", value: 18.2 },
-            ]}
-            title="Temperatura"
-            icon={<Cloud stroke="#17A34A" />}
-            unit="°C"
-          />
+      <HeroHeading scrollRef={ref} />
 
-          <InfoTabs
-            tabs={[
-              { key: "Minima", value: 50.2 },
-              { key: "Media", value: 71.1 },
-              { key: "Massima", value: 87.5 },
-            ]}
-            title="Umidità"
-            icon={<Droplet stroke="#17A34A" />}
-            unit="%"
-          />
-          <InfoTabs
-            title="Pressione"
-            icon={<ParkingMeter stroke="#17A34A" />}
-            tabs={[
-              { key: "Minima", value: 1005 },
-              { key: "Media", value: 1026 },
-              { key: "Massima", value: 1047 },
-            ]}
-            unit="hPa"
-          />
-          <Info
-            title="Precipitazini"
-            icon={<CloudRain stroke="#17A34A" />}
-            value={8.2}
-            unit="mm"
-            subtitle="+52.5% da ieri"
-          />
-        </ContainerCols>
-      </motion.div>
-      {/* WEEK INFO */}
+      <AnimationWrapper>
+        <HeroOverview
+          ref={ref}
+          lastUpdate={giorniSettimanaCorrente[6].toString()}
+          data={{
+            temp: {
+              curr: temperaturaUltimaRilevazione,
+              max: maxTemperaturaSettimanale[6],
+              min: minTemperaturaSettimanale[6],
+              delta: variazioneTemperaturaUltimoGiornoMisurazione,
+            },
+            humidity: {
+              curr: umiditaUltimaRilevazione,
+              max: maxUmiditaSettimanale[6],
+              min: minUmiditaSettimanale[6],
+              delta: variazioneUmiditaUltimoGiornoMisurazione,
+            },
+            rain: precipitazioniSettimanali[6],
+            pressure: {
+              curr: pressioneUltimaRilevazione,
+              max: maxPressioneSettimanale[6],
+              min: minPressioneSettimanale[6],
+              delta: variazionePressioneUltimoGiornoMisurazione,
+            },
+          }}
+        />
+      </AnimationWrapper>
+
       <div className="justify-center max-w-screen-lg px-5 m-auto mt-10 lg:grid-cols-5 lg:grid lg:gap-x-4 lg:px-0">
-        <WeekOverview />
-        {/* GRAPHS OVERVIEW */}
-        <Container className="order-1 w-full col-span-3 lg:h-full lg:mt-0">
-          {/* TABS FOR THE GRAPHS */}
-          <Tabs defaultValue="temperature">
-            {/* TITLE */}
-            <div className="flex items-center justify-between mb-4">
-              <h4 className="text-2xl font-semibold" ref={graph}>
-                Grafici della settimana
-              </h4>
-            </div>
-            <TabsList className="grid grid-cols-4 w-[100%] *:text-xs md:text-sm dark:bg-background/20">
-              <TabsTrigger value="temperature">Temperatura</TabsTrigger>
-              <TabsTrigger value="humidity">Umidità</TabsTrigger>
-              <TabsTrigger value="pressure">Pressione</TabsTrigger>
-              <TabsTrigger value="rain">Precipitazioni</TabsTrigger>
-            </TabsList>
-            <TabsContent value="temperature">
-              {" "}
-              <MyChart
-                IsInView={IsInViewGraph}
-                name="temperatura"
-                data={[10, 12, 7, 9, 8, 10, 15, 12]}
-                unit="°C"
-              />
-            </TabsContent>
-            <TabsContent value="humidity">
-              {" "}
-              <MyChart
-                IsInView={IsInViewGraph}
-                name="umidità"
-                data={[80, 74, 82, 64, 72, 76, 54, 91]}
-                unit="%"
-              />
-            </TabsContent>
-            <TabsContent value="pressure">
-              {" "}
-              <MyChart
-                IsInView={IsInViewGraph}
-                name="pressione"
-                data={[1004, 1020, 1012, 1018, 1008, 1028, 1011, 1012]}
-                unit="hPa"
-              />
-            </TabsContent>
-            <TabsContent value="rain">
-              {" "}
-              <MyChart
-                IsInView={IsInViewGraph}
-                name="precipitazioni"
-                data={[0, 2, 0, 0, 8, 10, 7]}
-                unit="mm"
-              />
-            </TabsContent>
-          </Tabs>
-        </Container>
+        <HeroWeek
+          min={minTemperaturaSettimanale}
+          max={maxTemperaturaSettimanale}
+          days={giorniGrafico}
+        />
+        <HeroGraphs
+          graphs={{
+            temp: temperaturaSettimanale,
+            humidity: umiditaSettimanale,
+            pressure: pressioneSettimanale,
+            rain: precipitazioniSettimanali,
+          }}
+          days={giorniGrafico}
+        />
       </div>
     </>
   );
