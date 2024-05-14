2d3296df3bb0f42e324f31f608da703be04bfaa1
leo0o7 <giardinoleonardo34@gmail.com>
2024-05-14 16:49:17 +0200

    initial route for last month query

---
+++ app/api/storico/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const res = await fetch(
+      "https://www.itisvallauri.net/meteo3//php/datiStorico.php",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // revalidate every 5 minutes
+        next: { revalidate: 300 },
+      }
+    );
+
+    const data = await res.json();
+
+    return new NextResponse(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Error", { status: 500 });
+  }
+}
